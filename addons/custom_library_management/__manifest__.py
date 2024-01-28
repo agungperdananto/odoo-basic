f390a1d547ca90a73a0cfaf9f865b560bc6dc18c
Agung  Perdananto <agung.perdananto@sirclo.com>
2024-01-28 10:12:26 +0700

    init custom library management

---
+++ addons/custom_library_management/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Module Custom Library Management',
+    'version': '0.0.1',
+    'category': 'library',
+    'summary': 'Custom Library Management Module',
+    'description': '''
+                Custom Library Management Module
+        ''',
+    'website': '',
+    'author': 'agung p',
+    'depends': ['web', 'base', 'product'],
+    'data': [
+        'security/ir.model.access.csv',
+        # 'views/custom_purchase_view.xml',
+        # 'views/custom_purchase_kanban_view.xml'
+
+        ],
+    'installlable': True,
+    'license': 'OEEL-1'
+    }
